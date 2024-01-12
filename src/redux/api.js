dec60ebe844603704cdd64b4a48c7df10c935f9f
OhEunTaek <giloet777@gmail.com>
2024-01-12 15:19:28 +0900

    유튜브 수정

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,39 +1,39 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const getFlickr = async (opt) => {
-  const key = 'fc53ebefe80f178c0979785406ddb3d5'
-  const num = 100
-  const extras = 'tags, description, views'
-  const method_search = 'flickr.photos.search'
-  const method_interest = 'flickr.interestingness.getList'
-  const method_user = 'flickr.people.getPhotos'
+	const key = '3180857568184579a8145861c8f2822d';
+	const num = 100;
+	const extras = 'tags, description, views';
+	const method_search = 'flickr.photos.search';
+	const method_interest = 'flickr.interestingness.getList';
+	const method_user = 'flickr.people.getPhotos';
 
-  let url = ''
-  if (opt.type === 'interest') {
-    url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&extras=${extras}&format=json&nojsoncallback=1`
-  }
+	let url = '';
+	if (opt.type === 'interest') {
+		url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&extras=${extras}&format=json&nojsoncallback=1`;
+	}
 
-  if (opt.type === 'search') {
-    url = `https://www.flickr.com/services/rest/?method=${method_search}&per_page=${num}&api_key=${key}&extras=${extras}&format=json&nojsoncallback=1&tags=${opt.tag}`
-  }
+	if (opt.type === 'search') {
+		url = `https://www.flickr.com/services/rest/?method=${method_search}&per_page=${num}&api_key=${key}&extras=${extras}&format=json&nojsoncallback=1&tags=${opt.tag}`;
+	}
 
-  if (opt.type === 'user') {
-    url = `https://www.flickr.com/services/rest/?method=${method_user}&per_page=${num}&api_key=${key}&extras=${extras}&format=json&nojsoncallback=1&tags1&user_id=${opt.owner}`
-  }
+	if (opt.type === 'user') {
+		url = `https://www.flickr.com/services/rest/?method=${method_user}&per_page=${num}&api_key=${key}&extras=${extras}&format=json&nojsoncallback=1&tags1&user_id=${opt.owner}`;
+	}
 
-  return await axios.get(url)
-}
+	return await axios.get(url);
+};
 
 export const getYoutube = async () => {
-  const key = 'AIzaSyDSdFyxbZ_BnbSiuPS3M6Ug9mM75LfRyJg'
-  const playList = 'PL_zwLMPR5YRLPV-Ni5MG2mds5p6RwupoD'
-  const num = 10
-  const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playList}&maxResults=${num}`
+	const key = 'AIzaSyDSdFyxbZ_BnbSiuPS3M6Ug9mM75LfRyJg';
+	const playList = 'PL_zwLMPR5YRLPV-Ni5MG2mds5p6RwupoD';
+	const num = 10;
+	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playList}&maxResults=${num}`;
 
-  return await axios.get(url)
-}
+	return await axios.get(url);
+};
 
 export const getMembers = async () => {
-  const url = process.env.PUBLIC_URL + '/DB/members.json'
-  return await axios.get(url)
-}
+	const url = process.env.PUBLIC_URL + '/DB/members.json';
+	return await axios.get(url);
+};
