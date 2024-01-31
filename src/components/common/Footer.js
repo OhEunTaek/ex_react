fdb27ec0c19a88880316ec109cb2078ba83fa485
OhEunTaek <giloet777@gmail.com>
2024-01-31 14:27:46 +0900

    footer와 global css수정

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,20 +1,22 @@
-import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { faEarthAsia } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Footer({ type }) {
   return (
     <footer className={type}>
       <ul>
-        <li>Privacy Policy</li>
+        <li>Policy</li>
         <li>Terms</li>
-        <li>Cookie Settings</li>
-        <li>Copyright &copy; 2022 Atlassian</li>
+        <li>Settings</li>
+        <li>Copyright &copy; 2024 Oh's Teacher</li>
       </ul>
 
       <form className="footer-form">
-        <FontAwesomeIcon icon={faGlobe} className="icon-globe" />
+        <FontAwesomeIcon icon={faEarthAsia} className="icon-globe" />
         <select name="language" id="">
           <option value="eng">Eng</option>
           <option value="kor">Kor</option>
+          <option value="jap">Jap</option>
+          <option value="chi">Chi</option>
         </select>
       </form>
     </footer>
